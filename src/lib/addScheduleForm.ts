00748da71b603077fd0853bfb417c402909ce827
Cat4848 <catalinrol@gmail.com>
2024-03-26 14:47:17 +0000

    add: form operation input

--- src/lib/addScheduleForm.ts
+++ src/lib/addScheduleForm.ts
@@ -9,10 +9,47 @@ function getNrOperations(e: Event) {
   const operationsValue = (e.target as HTMLSelectElement).value;
   const nrOperations = Number(operationsValue);
 
-  for (let i = 1; i < nrOperations; i++) {
-    const wrapper = document.createElement('div');
-    wrapper.className
+  for (let i = 1; i <= nrOperations; i++) {
+    const form = document.querySelector(".operations-form") as HTMLFormElement;
+    const wrapper = document.createElement("div");
+    wrapper.className = "input-group";
+    const label = document.createElement("label");
+    const input = document.createElement("input");
+    label.innerText = "Name";
+    label.htmlFor = `schedule-operation-${i}`;
+    input.id = `schedule-operation-${i}`;
+    input.name = `schedule-operation-${i}`;
+    wrapper.append(label, input);
+    form.append(wrapper);
   }
 }
 
-function createOperationsInput(nrOperations: number) {}
+function inputGroup(name: string) {
+  const wrapper = document.createElement("div");
+  wrapper.className = name;
+  return wrapper;
+}
+
+interface Label {
+  id: number;
+  name: string;
+  text: string;
+}
+function label({ id, name, text }: Label) {
+  const label = document.createElement("label");
+  label.innerText = text;
+  label.htmlFor = `${name}-${id}`;
+  return label;
+}
+
+interface Input {
+  id: number;
+  name: string;
+  type: string;
+}
+function input({ id, name, type }: Input) {
+  const input = document.createElement("input");
+  input.type = type;
+  input.id = `${name}-${id}`;
+  input.name = `${name}-${id}`;
+}
