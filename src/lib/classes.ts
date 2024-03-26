3922bba32b8d754c23f642cd8be7a7b373ab273a
Cat4848 <catalinrol@gmail.com>
2024-03-26 16:51:25 +0000

    add: schedule and operation classes

---
+++ src/lib/classes.ts
@@ -0,0 +1,27 @@
+import { ISchedule, IOperation } from "./definitions";
+
+export class Schedule implements ISchedule<IOperation> {
+  id: string;
+  name: string;
+  operations: IOperation[];
+
+  constructor(params: ISchedule<IOperation>) {
+    this.id = params.id;
+    this.name = params.name;
+    this.operations = params.operations;
+  }
+}
+
+export class Operation implements IOperation {
+  id: string;
+  name: string;
+  description: string;
+  duration: number;
+
+  constructor(params: IOperation) {
+    this.id = params.id;
+    this.name = params.name;
+    this.description = params.description;
+    this.duration = params.duration;
+  }
+}
