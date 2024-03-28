e6799d0648a1f43843caa68d6f8f067765feccc9
Cat4848 <catalinrol@gmail.com>
2024-03-28 20:49:28 +0000

    upd: removed commented code

--- src/tests/index.test.ts
+++ src/tests/index.test.ts
@@ -7,10 +7,4 @@ test("Schedule Viewer and Schedule Editor appear on screen", () => {
   setup();
   expect(screen.getByText("Schedule Viewer")).toBeDefined();
   expect(screen.getByText("Schedule Editor")).toBeDefined();
-  // const container = displaySchedulesNames(schedules);
-  // const header = document.querySelector(
-  //   "#schedule-viewer-header"
-  // ) as HTMLHeadingElement;
-  // const text = header.innerHTML;
-  // expect(text).toBe("Schedule Viewer");
 });
